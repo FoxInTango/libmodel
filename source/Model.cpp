944893e199e7455708e22f6881c50f99e06b50f3
sparrow.wsl <foxintango@hotmail.com>
2021-08-04 21:07:03 +0800

    NOTHING

--- source/Model.cpp
+++ source/Model.cpp
@@ -1,10 +1,28 @@
+/*
+ * libmodel
+ *
+ * Copyright (C) 2021 FoxInTango <foxintango@yeah.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 #include "../include/Model.h"
-//#include "json.hpp"
 using namespace foxintango;
-//using json = nlohmann::json;
-
-//void model_from_json(Model& m,json& j);
-//void walk_through_json(ModelElement* e,json& j);
 
 Model::Model() {
     setType(ModelElement::MET_MODEL);
@@ -26,8 +44,6 @@ Model::MODEL_STATUS Model::decode(char* buffer,const MODEL_FORMAT& format) {
         case MF_XML:{}break;
         case MF_YML:{}break;
         case MF_JSON:{
-            //json j = json::parse(buffer);
-            //model_from_json(*this,j);
         }break;
         default:break;
     }
